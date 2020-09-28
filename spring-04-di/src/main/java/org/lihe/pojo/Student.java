2e975036ae79865ad303d1eda0cd5a21670893b1
lihe2019 <900562554@qq.com>
2020-09-28 17:30:45 +0800

    第一次提交，现在到依赖注入了

---
+++ spring-04-di/src/main/java/org/lihe/pojo/Student.java
@@ -0,0 +1,92 @@
+package org.lihe.pojo;
+
+import java.util.*;
+
+public class Student {
+    private String name;
+    private Address address;
+    private String[] boooks;
+    private List<String> hobbys;
+    private Map<String, String> card;
+    private Set<String> games;
+    private String wife;
+    private Properties info;
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "name='" + name + '\'' +
+                ", address=" + address +
+                ", boooks=" + Arrays.toString(boooks) +
+                ", hobbys=" + hobbys +
+                ", card=" + card +
+                ", games=" + games +
+                ", wife='" + wife + '\'' +
+                ", info=" + info +
+                '}';
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Address getAddress() {
+        return address;
+    }
+
+    public void setAddress(Address address) {
+        this.address = address;
+    }
+
+    public String[] getBoooks() {
+        return boooks;
+    }
+
+    public void setBoooks(String[] boooks) {
+        this.boooks = boooks;
+    }
+
+    public List<String> getHobbys() {
+        return hobbys;
+    }
+
+    public void setHobbys(List<String> hobbys) {
+        this.hobbys = hobbys;
+    }
+
+    public Map<String, String> getCard() {
+        return card;
+    }
+
+    public void setCard(Map<String, String> card) {
+        this.card = card;
+    }
+
+    public Set<String> getGames() {
+        return games;
+    }
+
+    public void setGames(Set<String> games) {
+        this.games = games;
+    }
+
+    public String getWife() {
+        return wife;
+    }
+
+    public void setWife(String wife) {
+        this.wife = wife;
+    }
+
+    public Properties getInfo() {
+        return info;
+    }
+
+    public void setInfo(Properties info) {
+        this.info = info;
+    }
+}
